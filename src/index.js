2c875f4bb300b17c1d1be3ee3d55804c7670ea31
aleksandr <azotov86@gmail.com>
2017-02-28 23:26:21 +0500

    configurable function main

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import { makeBlinker, makePulsar, makeGosperGliderGun } from './createField';
+import { makePulsar } from './createField';
 import { step } from './gameOfLife';
 import render from './render';
 
 
-const main = (period) => {
-  const startField = makeGosperGliderGun();
-
-  const updateField = (field) => {
-    render(field, 'game-field-container');
-    const newField = step(field);
-
-    setTimeout(updateField, period, newField);
+const configurateMain = (builder, period) => {
+  const main = () => {
+    const startField = builder();
+    const updateField = (field) => {
+      render(field, 'game-field-container');
+      const newField = step(field);
+      setTimeout(updateField, period, newField);
+    };
+    setTimeout(updateField, period, startField);
   };
-  setTimeout(updateField, period, startField);
+  return main;
 };
 
-window.onload = main(100);
+window.onload = configurateMain(makePulsar, 150);
