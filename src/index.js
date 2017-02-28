ed2297c6edbaa638a7372950a90287a343bb5d0d
aleksandr <azotov86@gmail.com>
2017-02-28 13:16:19 +0500

    add function makeGosperGliderGun

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import { makePulsar } from './createField';
+import { makeBlinker, makePulsar, makeGosperGliderGun } from './createField';
 import { step } from './gameOfLife';
 import render from './render';
 
 
 const main = () => {
-  const startField = makePulsar();
+  const startField = makeGosperGliderGun();
 
   const updateField = (field) => {
     render(field, 'game-field-container');
     const newField = step(field);
 
-    setTimeout(updateField, 500, newField);
+    setTimeout(updateField, 50, newField);
   };
-  setTimeout(updateField, 500, startField);
+  setTimeout(updateField, 50, startField);
 };
 
 window.onload = main;
