9350aad723256e407465078bf8123f25d08cea0a
aleksandr <azotov86@gmail.com>
2017-02-28 22:54:47 +0500

    time of iteration assigned variable

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import { step } from './gameOfLife';
 import render from './render';
 
 
-const main = () => {
+const main = (period) => {
   const startField = makeGosperGliderGun();
 
   const updateField = (field) => {
     render(field, 'game-field-container');
     const newField = step(field);
 
-    setTimeout(updateField, 50, newField);
+    setTimeout(updateField, period, newField);
   };
-  setTimeout(updateField, 50, startField);
+  setTimeout(updateField, period, startField);
 };
 
-window.onload = main;
+window.onload = main(100);
