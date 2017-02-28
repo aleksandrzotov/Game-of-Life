d0b495db995f6e53c0aefde7ec8ddef077cd588b
aleksandr <azotov86@gmail.com>
2017-02-28 16:33:40 +0500

    render refactoring

--- src/render.js
+++ src/render.js
@@ -4,16 +4,11 @@ export default(array, id) => {
   newTable.className = 'game-field';
   const gameFieldContainer = document.getElementById(id);
   gameFieldContainer.appendChild(newTable);
-  for (let i = 0; i < array.length; i++) {
-    const line = array[i];
-    const curRow = newTable.insertRow(i);
-    for (let j = 0; j < line.length; j++) {
-      const curCell = curRow.insertCell(j);
-      if (line[j]) {
-        curCell.className = 'field-cell-black';
-      } else {
-        curCell.className = 'field-cell-white';
-      }
-    }
-  }
+  array.forEach((line) => {
+    const curRow = newTable.insertRow();
+    line.forEach((elem) => {
+      const curCell = curRow.insertCell();
+      curCell.className = elem ? 'field-cell-black' : 'field-cell-white';
+    });
+  });
 };
